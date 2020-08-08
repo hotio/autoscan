f796a1a803430807178b89e43b11d22ef0f056ed
hotio <26902309+hotio@users.noreply.github.com>
2020-08-08 14:53:15 +0200

    switch to gha

--- linux-amd64.Dockerfile
+++ linux-amd64.Dockerfile
@@ -17,3 +17,18 @@ COPY --from=builder /autoscan/autoscan ${APP_DIR}/autoscan
 RUN chmod 755 "${APP_DIR}/autoscan"
 
 COPY root/ /
+
+ARG LABEL_CREATED
+LABEL org.opencontainers.image.created=$LABEL_CREATED
+ARG LABEL_TITLE
+LABEL org.opencontainers.image.title=$LABEL_TITLE
+ARG LABEL_REVISION
+LABEL org.opencontainers.image.revision=$LABEL_REVISION
+ARG LABEL_SOURCE
+LABEL org.opencontainers.image.source=$LABEL_SOURCE
+ARG LABEL_VENDOR
+LABEL org.opencontainers.image.vendor=$LABEL_VENDOR
+ARG LABEL_URL
+LABEL org.opencontainers.image.url=$LABEL_URL
+ARG LABEL_VERSION
+LABEL org.opencontainers.image.version=$LABEL_VERSION
--- update.sh
+++ update.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-if [[ ${1} == "checkservice" ]]; then
-    SERVICE="http://service:3030/triggers/sonarr"
-    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL --header "Content-Type: application/json" --request POST --data '{"eventType": "Test","series": {"id": 1,"title": "Test Title","path": "C:\\testpath","tvdbId": 1234},"episodes": [{"id": 123,"episodeNumber": 1,"seasonNumber": 1,"title": "Test title","qualityVersion": 0}]}' ${SERVICE} > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
-    curl -fsSL --header "Content-Type: application/json" --request POST --data '{"eventType": "Test","series": {"id": 1,"title": "Test Title","path": "C:\\testpath","tvdbId": 1234},"episodes": [{"id": 123,"episodeNumber": 1,"seasonNumber": 1,"title": "Test title","qualityVersion": 0}]}' ${SERVICE} > /dev/null
-elif [[ ${1} == "checkdigests" ]]; then
+if [[ ${1} == "checkdigests" ]]; then
     mkdir ~/.docker && echo '{"experimental": "enabled"}' > ~/.docker/config.json
     image="hotio/base"
     tag="alpine"
     manifest=$(docker manifest inspect ${image}:${tag})
     [[ -z ${manifest} ]] && exit 1
-    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile && echo "${digest}"
-    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm.Dockerfile   && echo "${digest}"
-    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
 else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/cloudbox/autoscan/releases" | jq -r .[0].tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
-    sed -i "s/{AUTOSCAN_VERSION=[^}]*}/{AUTOSCAN_VERSION=${version}}/g" .drone.yml
+    sed -i "s/{AUTOSCAN_VERSION=[^}]*}/{AUTOSCAN_VERSION=${version}}/g" .github/workflows/build.yml
     echo "##[set-output name=version;]${version}"
 fi
