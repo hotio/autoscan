25e16a9b0dfeaef570f6baa2a6ce2cb2ed1c6c98
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 10:46:19 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/cloudbox/autoscan/releases/latest" | jq -re .tag_name)
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/cloudbox/autoscan/releases/latest" | jq -re '.tag_name')
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
