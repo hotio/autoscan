8e44f108a4d162736a87a4e36432dad8238e2301
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 07:28:22 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -2,8 +2,7 @@
 
 version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/cloudbox/autoscan/commits/master" | jq -r .sha)
 [[ -z ${version} ]] && exit 0
-old_version=$(jq -r '.version' < VERSION.json)
-changelog=$(jq -r '.changelog' < VERSION.json)
-[[ "${old_version}" != "${version}" ]] && changelog="https://github.com/cloudbox/autoscan/compare/${old_version}...${version}"
-version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .changelog = "'"${changelog}"'"' <<< "${version_json}" > VERSION.json
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version}" \
+    '.version = $version' <<< "${json}" > VERSION.json
