12bd316b132b26c42359e0e56b95690a9bdcf64f
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-09 08:29:34 +0100

    Remove GitHub authentication from version retrieval in update script [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -exuo pipefail
 
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/cloudbox/autoscan/commits/master" | jq -re '.sha')
+version=$(curl -fsSL "https://api.github.com/repos/cloudbox/autoscan/commits/master" | jq -re '.sha')
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
